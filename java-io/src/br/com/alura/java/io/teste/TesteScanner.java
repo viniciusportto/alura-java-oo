3bf574274e1535d4486e2e82d69a7a5bbf491c76
Vinicius Porto <viniciusportto@outlook.com>
2022-11-10 17:06:55 -0300

    teste scanners

---
+++ java-io/src/br/com/alura/java/io/teste/TesteScanner.java
@@ -0,0 +1,35 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteScanner {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(new File("java-io\\contas.csv"));
+
+        while (scanner.hasNextLine()){
+
+            String linha = scanner.nextLine();
+            System.out.println(linha);
+
+            Scanner linhaScanner = new Scanner(linha);
+            linhaScanner.useLocale(Locale.US);
+            linhaScanner.useDelimiter(",");
+
+            String tipoConta = linhaScanner.next();
+            int agencia = linhaScanner.nextInt();
+            int numero = linhaScanner.nextInt();
+            String titular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
+
+            System.out.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s: %08.2f%n",
+                    tipoConta, agencia, numero, titular, saldo);
+
+            linhaScanner.close();
+        }
+        scanner.close();
+    }
+
+}
