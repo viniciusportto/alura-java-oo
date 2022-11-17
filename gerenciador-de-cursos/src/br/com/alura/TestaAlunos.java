71339085003bda933da5d588aca3e9a894b5c3ba
Vinicius Porto <viniciusportto@outlook.com>
2022-11-16 21:52:14 -0300

    Set and HashSet

---
+++ gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
@@ -0,0 +1,32 @@
+package br.com.alura;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestaAlunos {
+
+    public static void main(String[] args) {
+
+        Set<String> alunos = new HashSet<>();
+        alunos.add("Vinicius Porto");
+        alunos.add("Isabela Santos");
+        alunos.add("Thiago Figueiredo");
+        alunos.add("Bruna Rodrigues");
+        alunos.add("Nathalia Leite");
+        alunos.add("Paulo Silveira");
+
+        System.out.println(alunos.size());
+
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+
+        alunos.forEach(aluno ->{
+            System.out.println(aluno);
+        });
+
+        System.out.println(alunos);
+
+    }
+
+}
