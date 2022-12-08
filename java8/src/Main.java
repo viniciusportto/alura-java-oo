39c057c713201c2926fc0cef3a7a5751b9fd38cd
Vinicius Porto <viniciusportto@outlook.com>
2022-12-07 22:51:06 -0300

    Streams

---
+++ java8/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Curso> cursos = new ArrayList<Curso>();
+        cursos.add(new Curso("Python", 45));
+        cursos.add(new Curso("JavaScript", 150));
+        cursos.add(new Curso("Java 8", 113));
+        cursos.add(new Curso("C", 55));
+
+        cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+
+       cursos.stream()
+               .filter(c -> c.getAlunos() > 100)
+               .forEach(c -> System.out.println(c.getNome()));
+
+       IntStream stream = cursos.stream()
+               .filter(c -> c.getAlunos() > 100)
+               .mapToInt(Curso::getAlunos);
+
+        int soma = cursos.stream()
+                .filter(c -> c.getAlunos() > 100)
+                .mapToInt(Curso::getAlunos)
+                .sum();
+
+    }
+}
