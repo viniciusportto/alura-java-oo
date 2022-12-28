96a9e516d12fad6c7abb3af8cf9727c8aa7eadf4
Vinicius Porto <viniciusportto@outlook.com>
2022-12-28 01:16:39 -0300

    TDD

---
+++ java-testes-unitarios-tdd/src/test/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,26 @@
+package test.br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.ReajusteService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ReajusteServiceTest {
+
+    @Test
+    public void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar(){
+        ReajusteService service = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Gabriel", LocalDate.now(), new BigDecimal("1000.00"));
+
+        service.concederReajuste(funcionario, Desempenho.A_Desejar);
+
+        Assertions.assertEquals(new BigDecimal("1030.00"),funcionario.getSalario());
+
+
+    }
+
+}
