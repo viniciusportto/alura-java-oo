5f4bbe74fe9e7b3bd637ff9f108d022910c9148c
Vinicius Porto <viniciusportto@outlook.com>
2022-12-28 01:50:24 -0300

    Refactor the service and enums

--- java-testes-unitarios-tdd/src/main/java/br/com/alura/tdd/modelo/Desempenho.java
+++ java-testes-unitarios-tdd/src/main/java/br/com/alura/tdd/modelo/Desempenho.java
@@ -1,5 +1,24 @@
 package br.com.alura.tdd.modelo;
 
+import java.math.BigDecimal;
+
 public enum Desempenho {
-    BOM, OTIMO, A_Desejar
+    BOM {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.15");
+        }
+    }, OTIMO {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.20");
+        }
+    }, A_Desejar {
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.03");
+        }
+    };
+
+    public abstract BigDecimal percentualReajuste();
+
 }
--- java-testes-unitarios-tdd/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ java-testes-unitarios-tdd/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -7,15 +7,8 @@ import java.math.BigDecimal;
 
 public class ReajusteService {
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho == Desempenho.A_Desejar) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.reajustarSalario(reajuste);
-        } else if (desempenho == Desempenho.BOM) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.reajustarSalario(reajuste);
-        } else {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.reajustarSalario(reajuste);
-        }
+        BigDecimal percentual = desempenho.percentualReajuste();
+        BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+        funcionario.reajustarSalario(reajuste);
     }
 }
